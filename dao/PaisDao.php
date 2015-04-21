62119d5428ad36cc18d75ff30369baa4c03fa73b
Ricardo Santiago Morais <santhiagoxp3600@hotmail.com>
2015-04-20 23:15:06 -0300

    Entidade Pais

--- dao/PaisDao.php
+++ dao/PaisDao.php
@@ -1,1 +1,15 @@
+<?php
+class PaisDao 
+{
+	
+        public function InserirCategoria(Pais $pais) 
+        {
+            $conexao = new PDOUtil();
+            $insert = $conexao->getStance()->prepare("INSERT INTO pais(nome_pais) VALUES(:nome_pais)");
+            $insert->bindValue(":nome_pais", $pais->getNome_pais());          
+            $insert->execute();
+        }
+        
+
+}
 
--- entity/Pais.php
+++ entity/Pais.php
@@ -1,1 +1,32 @@
 <?php
+
+class Pais
+{
+    private $idpais;
+    private $nome_pais;
+
+    public function __construct($nome_pais) 
+    {
+        $this->nome_pais = $nome_pais;    
+    }
+    
+    public function getIdpais()
+    {
+        return $this->idpais;
+    }
+    public function setIdpais($idpais)
+    {
+        return $this->idpais = $idpais;
+    }
+   
+    public function getNome_pais()
+    {
+        return $this->nome_pais;
+    }
+    public function setNomepais()
+    {
+        return $this->nome_pais = $nome_pais;
+    }
+    
+    
+}
